01587d80674e6c62cc4d100eac100e5f5cce3631
Anadela Hernandeez <anadelahernandez74@gmail.com>
2024-08-20 18:41:23 -0500

    conexion netlify

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const corsOptions = {
+    origin: 'https://prueba-tecnica-fenix.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.static('./public'));
 
 const rutaPrincipal = express.Router();
